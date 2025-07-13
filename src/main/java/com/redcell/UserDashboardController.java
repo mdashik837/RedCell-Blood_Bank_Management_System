679fa7eda0086ff2a541591b0934446a78e3f3c7
Shafayat Yeamin Jian <jian.saidpur318@gmail.com>
2025-07-13 07:04:33 +0600

    Add files via upload
    Main java class and controller class update

---
+++ src/main/java/com/redcell/UserDashboardController.java
@@ -0,0 +1,14 @@
+package com.redcell;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserDashboardController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
